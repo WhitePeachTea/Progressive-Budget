c56bf9e9af135ac1ffbf923c931bcbf4a50c6e00
WhitePeachTea <penglei2123@gmail.com>
2020-03-07 00:28:47 -0500

    mlab connect

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://heroku_qfb4qstd:llqq0h7qhr0mu9h34j6kp172q@ds131676.mlab.com:31676/heroku_qfb4qstd", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
